2ba9a52981683e65e0ff08ae58810f40a5d60cc7
AngeNicole <angenicolemukundwa22@gmail.com>
2024-09-28 00:58:14 +0400

    fixing bugs

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """This is the amenity class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -14,3 +13,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
 
+# place_amenities = relationship('Place', secondary='place_amenity')
