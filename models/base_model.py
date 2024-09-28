e2e8a6a1c81fb6c4bbb331a00beceab782e28c2b
Aimee-Gloire <i.aimeegloi@alustudent.com>
2024-09-28 10:25:58 +0200

    test fixes in basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,16 @@ from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
 
+
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
 
     # initialize database columns
-    id = Column(String(60), nullable=False, primary_key=True, default=lambda: str(uuid.uuid4()))
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=created_at)
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -29,22 +30,20 @@ class BaseModel:
             updated_at: updated date
         """
         if kwargs:
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
 
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
             if 'created_at' not in kwargs:
-                self.created_at = datetime.utcnow()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.utcnow()
+                self.created_at = self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
@@ -55,14 +54,14 @@ class BaseModel:
             type(self).__name__, self.id, self.__dict__)
 
     def __repr__(self):
-        """return a string representation
+        """return a string representaion
         """
         return self.__str__()
 
     def save(self):
         """updates the public instance attribute updated_at to current
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
@@ -72,7 +71,7 @@ class BaseModel:
         models.storage.delete(self)
 
     def to_dict(self):
-        """creates dictionary of the class and returns
+        """creates dictionary of the class  and returns
         Return:
             returns a dictionary of all the key values in __dict__
         """
