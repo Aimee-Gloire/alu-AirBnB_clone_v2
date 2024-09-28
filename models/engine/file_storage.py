93e02c449def4f21484f0ed510b71a24855d7891
Aimee-Gloire <i.aimeegloi@alustudent.com>
2024-09-28 09:32:51 +0200

    w293/w292/w291 bug fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 """Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
-from models.amenity import Amenity  
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
